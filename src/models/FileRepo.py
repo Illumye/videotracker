fdd0ee55d433155c3a49bdf038122938fcd3db7e
Angy Seguy <angy.seguy@etu.u-bordeaux.fr>
2024-03-07 08:34:17 +0100

    Correction Point.py et FileRepo.py

--- src/models/FileRepo.py
+++ src/models/FileRepo.py
@@ -1,9 +1,15 @@
-from collections import namedtuple
-
-PointBase = namedtuple('PointBase', ['x', 'y'])
-
-class Point(PointBase):
-    def getX(self):
-        return self.x
-    def getY(self):
-        return self.y
+class FileRepo:
+    def __init__(self, file_name, path):
+        self.__file_name = file_name
+        self.__path = path
+        
+    def transformData2CSV(self, temps: list, points: list, sep: str) -> str:
+        csv_data = ""
+        for time, point in zip(temps, points):
+            csv_data += f"{time}{sep}{point.getX()}{sep}{point.getY()}"
+        return csv_data
+    
+    def export2CSV(self, temps: list, points: list, sep: str):
+        csv_data = self.transformData2CSV(temps, points, sep)
+        with open(f"{self.__path}/{self.__file_name}", "w") as file:
+            file.write(csv_data)
